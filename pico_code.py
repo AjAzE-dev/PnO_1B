02cf2314768950269cde81196bc6e19e3f8ba3a6
AjAzE <93143063+AjAzE-dev@users.noreply.github.com>
2026-03-09 17:33:47 +0100

    update pico code

--- pico_code.py
+++ pico_code.py
@@ -3,8 +3,26 @@ import wifi
 import time
 import digitalio
 import board
+from analogio import AnalogIn
 from adafruit_httpserver import Server, Request, Response, GET, Websocket
 
+def draai_links():
+    while meetpin_links_voor != zwart and meetpin_rechts_voor != zwart:
+        right_power.value = True
+        left_power.value = True
+        right_direction.value = True
+        left_direction.value = False
+    right_power.value = False
+    left_power.value = False
+
+def draai_rechts():
+    while meetpin_links_voor != zwart and meetpin_rechts_voor != zwart:
+        right_power.value = True
+        left_power.value = True
+        right_direction.value = False
+        left_direction.value = True
+    right_power.value = False
+    left_power.value = False
 
 SSID = "PICO-TEAM-110"  #Verander X naar groepsnummer
 PASSWORD = "wachtwoord110"  #Verander voor veiligheidsredenen
@@ -20,6 +38,10 @@ left_power.direction = digitalio.Direction.OUTPUT
 
 left_direction = digitalio.DigitalInOut(board.GP3) 
 left_direction.direction = digitalio.Direction.OUTPUT
+
+meetpin_rechts_voor = AnalogIn(board.GP26)
+meetpin_links_voor = AnalogIn(board.GP27)
+meetpin_achter = AnalogIn(board.GP28)
 
 wifi.radio.start_ap(ssid=SSID, password=PASSWORD)
 
@@ -54,6 +76,17 @@ while True:
             print("RECEIVED:", repr(data))
 
             if data.strip() == "move_forward":
+                right_power.value = True
+                right_direction.value = True
+                left_power.value = True
+                left_direction.value = True
+                if meetpin_achter == "zwart" and volgende_vakje == links:
+                    draai_links()
+                elif meetpin_achter == "zwart" and volgende_vakje == rechts:
+                    draai_rechts()
+            
+            """
+            if data.strip() == "move_forward":
                 print("TURNING MOTOR ON")
                 right_power.value = True
                 right_direction.value = True
@@ -77,5 +110,6 @@ while True:
             if data.strip() == "stop":
                right_power.value = False
                left_power.value = False 
+            """
 
     time.sleep(0.1)
